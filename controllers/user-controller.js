571b142ed8d2fd1ceb34a980b461ad1c9ca56247
yann-fk-21 <168545866+yann-fk-21@users.noreply.github.com>
2024-05-11 17:23:16 +0000

    added protected routes and add-post feature

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,11 +1,20 @@
-const User = require('../models/user');
+// const User = require('../models/user');
+const Post = require('../models/post');
 
-exports.getHomepage = (req, res, next) => {
-    res.render('global/homepage',{
-        pageTitle:'Home',
-        path:'/',
-        isAuthenticated: req.session.isLoggedIn
-    })
+exports.getHomepage = async (req, res, next) => {
+
+    try {
+        const posts = await Post.find();
+
+        res.render('global/homepage',{
+            pageTitle:'Home',
+            path:'/',
+            isAuthenticated: req.session.isLoggedIn,
+            posts: posts
+        })
+    } catch(err) {
+        console.log(err);
+    }
 }
 
 exports.getShare = (req, res, next) => {
@@ -14,4 +23,22 @@ exports.getShare = (req, res, next) => {
         path:'/share',
         isAuthenticated: req.session.isLoggedIn
     })
+}
+
+exports.postShare = async (req, res, next) => {
+    
+    try {
+
+    const title = req.body.title ;
+    const description = req.body.description;
+    const imageUrl = req.body.imageUrl;
+    const userId = req.session.user._id;
+
+    const newPost = new  Post({title: title, description: description, imageUrl: imageUrl, userId: userId});
+    await newPost.save();
+    res.redirect('/');
+    } catch(err) {
+        console.log(err);
+    }
+
 }
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/user-controller');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 router.get('/', userController.getHomepage);
-router.get('/share', userController.getShare);
+router.get('/share', isAuth, userController.getShare);
+router.post('/share', isAuth, userController.postShare);
 
 module.exports = router;
