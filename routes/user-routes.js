932656685bdf263f9b3f7785dc5ae3620b7b1301
yann-fk-21 <168545866+yann-fk-21@users.noreply.github.com>
2024-05-19 12:51:12 +0000

    added validation

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,11 +1,20 @@
 const express = require('express');
+
 const userController = require('../controllers/user-controller');
 const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 router.get('/', userController.getHomepage);
+
 router.get('/share', isAuth, userController.getShare);
+
+router.get('/edit-post/:postId', isAuth, userController.getPost);
+
 router.post('/share', isAuth, userController.postShare);
+
+router.post('/edit-post', isAuth, userController.editPost);
+
+router.post('/delete/:postId', isAuth, userController.postDelete);
 
 module.exports = router;
